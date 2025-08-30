3c5f2d91cf69392215c2077e0c17cf1744aa7828
FLTProjectGroupe2025 <techprojet8@gmail.com>
2025-08-30 06:51:14 +0300

    En-block.c

--- FLT_Project.c
+++ FLT_Project.c
@@ -4,10 +4,11 @@
   * @author Harimanantsoa Nomenjanahary Lafatriniaina
   * @author ANDRIAMAHEFA Hanitriniala Miandrisoa Fanantenana
   * @author ANDRIAMIANDRISOA Tolotra Maminiaina Roderic
-  * @brief Un programme qui d'apprentissage de l'anglais à partir de la langue française.
+  * @brief Un programme d'apprentissage de l'anglais à partir de la langue française (Ce ne sera un apprentissage proffessionnel ou complet, seulement quelques bases)
   * @details Ses fonctionnalités sont:
        *1-Les utilisateurs doivent s'inscrire avant de commencer à utiliser le programme pour la première fois. Ils doivent entrer leurs noms et créer leurs propre mots de passe. Ces informations seront ensuite stockées pour que la prochaine fois qu'ils utiliseront le programme, ils n'auront plus qu'à s'authentifier.
        *2-Ce programme fonctionnera sous forme de parcours d'apprentissage, découpé en plusieurs mini-niveaux dont chacun est composé d'une leçon et d'un test. L'accès aux leçons est progressif et séquenciel: pour débloquer une nouvelle leçon, l'utilisateur devra compléter celle qui la précède, et il pourra aussi revoir autant de fois qu'il veut les léçons déjà débloquées. Les progressions de chaque utilisateur inscrit dans ce programme seront alors stockéés spécifiquement. Comme ça à chaque fois qu'un utilisateur lance le programme, les niveaux qu'il a débloqués resteront les mêmes que la dernière fois que "lui", il a lancé le programme.
        *3-Ce programme créera un gestionnaire de temps à l'inscription d'un utilisateur. Il devra informer au programme les jours de la semaine où il sera libre et pendant combien de temps par jour il aura. Le programme enregistera ensuite ces informations et lui préscrira combien de leçons par jour il devrait débloquer et après combien de temps il aura avant de terminer son apprentissage. Ce programme lui donnera aussi des avertissements s'il ne suit pas correctement l'emploi de temps préscrit.
-      *4-Autre: le programme permettra aussi une option de mini-jeux afin d'aider l'utisateur à réviser ses leçons: des devinettes, des words matching, des quiz,...
+       *4-Autre: le programme permettra aussi une option de mini-jeux afin d'aider l'utisateur à réviser ses leçons: des devinettes, des words matching, des quiz,...
+  * @brief Le programme se concentrera surtout sur ces fonctionnalités que sur les cours, mais bien entendu, on fera du mieux qu'on peut pour donner des leçons propres et profitables.    
   */
